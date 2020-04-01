bd0bfdca1bf7ef66c76282bad88b134474f8d783
seihan1234 <zedmain01@abv.bg>
2020-04-01 17:44:03 +0300

    seihan123

---
+++ zad1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace zad1
+{
+    class Program
+    {
+        private static int col;
+
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                var line = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+                for (int col = 0; col < cols; col++) matrix[rows, cols] = line[col];
+                
+
+                
+            }
+            int maxSum = 0, maxRow = -1, maxCol = -1;
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    int tempSum = matrix[row, col] + matrix[row + 1, col] +
+                                  matrix[row, col + 1] + matrix[row + 1, col + 1];
+                        if(tempSum > maxSum)
+                    { 
+                        maxSum = tempSum;
+                        maxRow = row;
+                        maxCol = col;
+                        Console.WriteLine("Sum {0} at [{1},{2}]", maxSum, maxRow, maxCol);
+
+
+                    }
+
+                } 
+            }
+            for (int row = maxRow; row < maxRow; row++)
+            {
+                for (int col = maxCol; col < maxCol + 2; col++);
+                Console.Write(matrix[row, col] +" ");
+                Console.WriteLine();
+                
+
+                
+            }
+        }
+    }
+}
---
+++ zad3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace seihan_zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[rows,cols]; 
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+            }
+
+            for (int col = 0; col < cols; col++)
+            {
+                int min = 1000;
+                for (int row = 0; row < rows; row++)
+                {
+                    if (matrix[row, col] < min) min = matrix[row, col];
+                }
+                Console.Write("{0}", min);
+            }
+        }
+    }
+}
